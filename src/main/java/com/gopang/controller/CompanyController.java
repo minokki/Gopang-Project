79fa03fdc2a62bb2e3a40e3e12299234c7293283
minokki <regular2644@gmail.com>
2023-12-12 10:15:09 +0900

    fix:company controller

--- src/main/java/com/gopang/controller/CompanyController.java
+++ src/main/java/com/gopang/controller/CompanyController.java
@@ -10,19 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CompanyController {
     /* 회사소개 페이지 이동 */
     @GetMapping("/company/greeting")
-    public String get_company_greeting() {
+    public String get_company_greeting(@CurrentUser Account account, Model model) {
+        if( account != null) {
+            model.addAttribute(account);
+        }
         return "company/company_greeting";
     }
 
     /* 회사소개 페이지 이동 */
     @GetMapping("/company/info")
-    public String get_company_info() {
+    public String get_company_info(@CurrentUser Account account, Model model) {
+        if( account != null) {
+            model.addAttribute(account);
+        }
         return "company/company_info";
     }
     /* 오시는길 페이지 이동 */
     @GetMapping("/company/map")
-    public String get_company_map() {
-
+    public String get_company_map(@CurrentUser Account account, Model model) {
+        if( account != null) {
+            model.addAttribute(account);
+        }
         return "company/company_companyMap";
     }
 }
