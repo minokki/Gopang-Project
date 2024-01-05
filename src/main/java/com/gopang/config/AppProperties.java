e04505e8f721575165fb106aef18ceeb58d96261
minokki <regular2644@gmail.com>
2024-01-05 14:58:52 +0900

    fix: properties host change

--- src/main/java/com/gopang/config/AppProperties.java
+++ src/main/java/com/gopang/config/AppProperties.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("app")
 public class AppProperties {
 
-    private String host;
+    private String host = "https://greeners.co.kr/";
 }
 
