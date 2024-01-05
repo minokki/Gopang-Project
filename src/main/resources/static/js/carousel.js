972647baa3e9c97fa85914e08bee5af6a18e7412
minokki <regular2644@gmail.com>
2024-01-05 14:03:10 +0900

    feat: css modi, img change

--- src/main/resources/static/js/carousel.js
+++
@@ -1,9 +0,0 @@
-//  캐러셀이 마우스 오버 시 일시 중지되는 동작을 제어
-$(document).ready(function(){
-    var myCarousel = document.querySelector('.carouselExampleIndicators')
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        pause: false
-    });
-});
-
-
